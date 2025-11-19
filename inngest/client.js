99d9caa7314fe47a8ad7422237734dae593bff99
Shubham Kumar <shubhamkumar@Shubhams-MacBook-Pro.local>
2025-11-19 18:10:08 +0530

    Add Inngest event key configuration

--- inngest/client.js
+++ inngest/client.js
@@ -1,3 +1,6 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "ticketing-system" });
+export const inngest = new Inngest({ 
+  id: "ticketing-system",
+  eventKey: process.env.INNGEST_EVENT_KEY,
+});
